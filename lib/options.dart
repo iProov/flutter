176def7b1fb90fd03d8691c49fb54f19d5c34c78
Andrea Bizzotto <bizz84@gmail.com>
2021-09-10 09:12:24 +0100

    Add comments to options.dart

--- lib/options.dart
+++ lib/options.dart
@@ -4,6 +4,26 @@ import 'dart:ui';
 import 'package:image/image.dart' hide Color;
 import 'package:iproov_flutter/enums.dart';
 
+// TODO: For all the classes in this file, identify which properties need to be null
+// and which ones don't (non-nullable should be the default).
+// Then, declare them all as final and add a constructor.
+//
+// Example:
+// class LivenessAssuranceUiOptions {
+//   LivenessAssuranceUiOptions({
+//     required this.primaryTintColor,
+//     required this.secondaryTintColor,
+//   });
+//   final Color primaryTintColor;
+//   final Color secondaryTintColor;
+
+//   Map<String, dynamic> toJson() => removeNulls({
+//         'primary_tint_color': primaryTintColor.hex,
+//         'secondary_tint_color': secondaryTintColor.hex,
+//       });
+// }
+// Then the [removeNulls] function may become redundant
+
 class Options {
   UiOptions ui = UiOptions();
   NetworkOptions network = NetworkOptions();
@@ -17,9 +37,8 @@ class Options {
 }
 
 class UiOptions {
-  GenuinePresenceAssuranceUiOptions genuinePresenceAssurance =
-      GenuinePresenceAssuranceUiOptions();
-  LivenessAssuranceUiOptions livenessAssurance = LivenessAssuranceUiOptions();
+  final genuinePresenceAssurance = GenuinePresenceAssuranceUiOptions();
+  final livenessAssurance = LivenessAssuranceUiOptions();
   Filter? filter;
   Color? lineColor;
   Color? backgroundColor;
@@ -37,7 +56,7 @@ class UiOptions {
   int? activityCompatibilityRequestCode;
 
   Map<String, dynamic> toJson() {
-    var map = removeNulls({
+    return removeNulls({
       'genuine_presence_assurance': genuinePresenceAssurance.toJson(),
       'liveness_assurance': livenessAssurance.toJson(),
       'filter': filter?.stringValue,
@@ -51,17 +70,10 @@ class UiOptions {
       'orientation': orientation?.stringValue,
       'activity_compatibility_request_code': activityCompatibilityRequestCode,
       'close_button_tint_color': closeButtonTintColor?.hex,
+      if (logoImage != null) 'logo_image': base64.encode(encodePng(logoImage!)),
+      if (closeButtonImage != null)
+        'close_button_image': base64.encode(encodePng(closeButtonImage!)),
     });
-
-    if (logoImage != null) {
-      map['logo_image'] = base64.encode(encodePng(logoImage!));
-    }
-
-    if (closeButtonImage != null) {
-      map['close_button_image'] = base64.encode(encodePng(closeButtonImage!));
-    }
-
-    return map;
   }
 }
 
